147a89152412d34185004b5f6d0c90a53d024a7c
wendyzhao-zwx <wendyzhao62@gmail.com>
2021-03-07 01:56:49 -0500

    add cards

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> titles = <String>[
+    "  Daily Activity",
+    "  Exercise",
+    "  Body Measurements",
+    "  Body Condition",
+    "  Health Data",
+    "  Sleep",
+    "  Period",
+  ];
+
+  final List<IconData> icons = <IconData>[
+    Icons.emoji_people_rounded,
+    Icons.local_fire_department,
+    Icons.accessibility,
+    Icons.sentiment_neutral_rounded,
+    Icons.insert_chart,
+    Icons.bedtime_rounded,
+    Icons.invert_colors_rounded,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,29 +36,88 @@ class _HomePageState extends State<HomePage> {
         title: Text('Home'),
       ),
       body: ListView.builder(
-          itemCount: 500,
+          itemCount: 7,
           itemBuilder: (context, index) {
-            return Card(
-                color: index % 2 == 0 ? Colors.grey.shade600 : null,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(children: <Widget>[
-                    Text(
-                      'Title of home block ${index + 1}',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: index % 2 == 0 ? Colors.white : null,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text('some great and interesting text',
-                        style: TextStyle(
-                          color: index % 2 == 0 ? Colors.white : null,
-                        ))
-                  ]),
-                ));
+            return Container(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                height: 100,
+                width: double.maxFinite,
+                child: Card(
+                    child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 32.0,
+                    top: 16.0,
+                    right: 16.0,
+                    bottom: 16.0,
+                  ),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            child: RichText(
+                              text: TextSpan(
+                                children: [
+                                  WidgetSpan(
+                                    child:
+                                        Icon(icons[index]), //cardIcon(index)),
+                                  ),
+                                  TextSpan(
+                                    text: titles[index], //cardTitle(index),
+                                    //textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      color: Colors.black,
+                                      // fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            // child: Text(
+                            //   cardTitle(index),
+                            //   //textAlign: TextAlign.center,
+                            //   style: TextStyle(
+                            //     fontSize: 25,
+                            //     // fontWeight: FontWeight.bold,
+                            //   ),
+                            // ),
+                          ),
+                        ),
+                      ]),
+                )));
           }),
+      // body: Container(
+      //   padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+      //   height: 125,
+      //   width: double.maxFinite,
+      //   child: Card(
+      //       shape: RoundedRectangleBorder(
+      //         borderRadius: BorderRadius.circular(15.0),
+      //       ),
+      //       child: Padding(
+      //         padding: const EdgeInsets.all(20.0),
+      //         child: Column(
+      //             mainAxisAlignment: MainAxisAlignment.center,
+      //             children: <Widget>[
+      //               Align(
+      //                 alignment: Alignment.center,
+      //                 child: Container(
+      //                   child: Text(
+      //                     "Should be left",
+      //                     textAlign: TextAlign.center,
+      //                     style: TextStyle(
+      //                       fontSize: 25,
+      //                       // fontWeight: FontWeight.bold,
+      //                     ),
+      //                   ),
+      //                 ),
+      //               ),
+      //               SizedBox(height: 10),
+      //             ]),
+      //       )),
+      // ),
     );
   }
 }
